7014c69bc0e0246fe71c374023b00b369fd003ac
Adrian Agnic <adrian.agnic@hotmail.com>
2017-08-11 12:59:48 -0700

    start newsletter

--- SonOfCod/Controllers/MarketingController.cs
+++ SonOfCod/Controllers/MarketingController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SonOfCod.Models;
 using Microsoft.AspNetCore.Identity;
+using SonOfCod.ViewModels;
 
 namespace SonOfCod.Controllers
 {
@@ -32,5 +33,46 @@ namespace SonOfCod.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> AdminLogin(LoginViewModel model)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult AdminRegister()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AdminRegister(RegisterViewModel model)
+        {
+            var admin = new ApplicationUser { UserName = model.Email };
+            IdentityResult result = await _userManager.CreateAsync(admin, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index");
+        }
     }
 }
